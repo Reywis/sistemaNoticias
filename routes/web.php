116553b2229f0ce787a4b10493412a5b3146d963
reywis <reywisveliz@gmail.com>
2025-04-15 00:06:03 -0500

    subiendo mas actualizaciones

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoticiaController;
 use App\Models\Noticia;
+use Maatwebsite\Excel\Facades\Excel;
 
 /*
 |--------------------------------------------------------------------------
