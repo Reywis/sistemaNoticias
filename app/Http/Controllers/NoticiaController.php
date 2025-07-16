e5116c32d4596c12d403f9aa2d709e5a305a9b24
reywis <reywisveliz@gmail.com>
2025-07-16 16:49:13 -0500

    subiendo mejoras para el excel

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Noticia;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class NoticiaController extends Controller
